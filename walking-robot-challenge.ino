88ae41edced63fc7e0501f3602bab6e4458da123
Eric Zhang <ericbkzhang@gmail.com>
2025-04-02 11:52:49 -0400

    Add some code to hopefully make a robot walk

--- walking-robot-challenge.ino
+++ walking-robot-challenge.ino
@@ -5,9 +5,15 @@ void setup() {
     // Set the motor pins
     pinMode(RIGHT_MOTOR, OUTPUT);
     pinMode(LEFT_MOTOR, OUTPUT);
-
 }
 
 void loop() {
+    digitalWrite(RIGHT_MOTOR, HIGH);
+    digitalWrite(LEFT_MOTOR, LOW);
+    delay(1000);
+
+
     digitalWrite(RIGHT_MOTOR, LOW);
+    digitalWrite(LEFT_MOTOR, HIGH);
+    delay(1000);
 }
