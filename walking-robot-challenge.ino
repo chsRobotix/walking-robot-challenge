933941b346d87481d8f9e41323127c4d0cc7ebfa
Eric Zhang <ericbkzhang@gmail.com>
2025-04-09 11:57:52 -0400

    Add foot motors

--- walking-robot-challenge.ino
+++ walking-robot-challenge.ino
@@ -1,19 +1,24 @@
-#define RIGHT_MOTOR 2
-#define LEFT_MOTOR 12
+const int RIGHT_LEG_MOTOR = 2;
+const int RIGHT_FOOT_MOTOR 4;
+const int LEFT_LEG_MOTOR = 12;
+const int LEFT_FOOT_MOTOR = 10;
 
 void setup() {
     // Set the motor pins
-    pinMode(RIGHT_MOTOR, OUTPUT);
-    pinMode(LEFT_MOTOR, OUTPUT);
+    pinMode(RIGHT_LEG_MOTOR, OUTPUT);
+    pinMode(RIGHT_FOOT_MOTOR, OUTPUT);
+    pinMode(LEFT_LEG_MOTOR, OUTPUT);
+    pinMode(LEFT_FOOT_MOTOR, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(RIGHT_MOTOR, HIGH);
-    digitalWrite(LEFT_MOTOR, LOW);
+    digitalWrite(RIGHT_LEG_MOTOR, HIGH);
+    digitalWrite(RIGHT_FOOT_MOTOR, HIGH);
+    digitalWrite(LEFT_LEG_MOTOR, LOW);
     delay(1000);
 
-
-    digitalWrite(RIGHT_MOTOR, LOW);
-    digitalWrite(LEFT_MOTOR, HIGH);
+    digitalWrite(LEFT_LEG_MOTOR, HIGH);
+    digitalWrite(LEFT_FOOT_MOTOR, HIGH);
+    digitalWrite(RIGHT_LEG_MOTOR, LOW);
     delay(1000);
 }
